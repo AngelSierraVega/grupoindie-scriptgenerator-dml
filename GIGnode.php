aeb45978eac8b3eebcd17860b1b8468eee45f5d7
Angel <Angel@AngelPC>
2016-12-04 18:27:14 -0600

    21016 12 04 clean project start

---
+++ GIGnode.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * Copyright (C) 2016 Angel Sierra Vega. Grupo INDIE.
+ *
+ * This software is protected under GNU: you can use, share, study and 
+ * modify it but not distribute it under the terms of the GNU General 
+ * Public License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ */
+
+/**
+ * Represents a node element
+ */
+class GIGnode {
+    //put your code here
+}
