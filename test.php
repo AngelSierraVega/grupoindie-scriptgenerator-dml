5e53acbe9f43d0f5745b0399a017be982559c716
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2017-03-31 20:34:34 -0600

    Updated node manipulation

--- test.php
+++ test.php
@@ -8,36 +8,6 @@
  * License, or (at your option) any later version.
  */
 require_once 'src/main.php';
-echo GIgenerator\DML\Node::Simple("node") . " || "; ?><node></node><?php echo "\n";
-echo GIgenerator\DML\Node::Simple("node",["attr"=>"val"]) . " || "; ?><node attr='val'></node><?php echo "\n";
-echo GIgenerator\DML\Node::Simple("node",["node"=>null]) . " || "; ?><node attr></node><?php echo "\n";
-echo GIgenerator\DML\Node::Simple("node",[],["content"]) . " || "; ?><node>content</node><?php echo "\n";
-echo "\n";
-echo "--- Example 3: Closed node \n";//Should render: <node_closed />
-echo GIgenerator\DML\Node::Closed("node_closed");
-echo "\n";
-echo "\n";
-echo GIgenerator\DML\Node::Simple("parent",[],[GIgenerator\DML\Node::Simple("child")]) . " || "; ?><parent><child></child></parent><?php echo "\n";
-echo GIgenerator\DML\Node::EmptyNode("node_empty") . " || "; ?><node_empty><?php echo "\n";
-echo GIgenerator\DML\Node::ContentOnly([GIgenerator\DML\Node::Simple("node1"),GIgenerator\DML\Node::Simple("node2")]) . " || ";?><node1></node1><node2></node2><?php
-echo "\n";
-echo "------------------\n";
-echo "----Functions-----\n";
-echo "------------------\n";
-
-$example = GIgenerator\DML\Node::Simple("node",["attr"=>"val"]);
-echo $example . " || "; ?><node attr='val'></node><?php echo "\n";
-$example->unsetAttribute("attr");
-echo $example . " || "; ?><node></node><?php echo "\n";
-$example->setTag("changedTag");
-echo $example . " || "; ?><changedTag></changedTag><?php echo "\n";
 
 
 
-
-//GIndie\DML\Node\
-
-//    function displayError(Exception $e) {
-//        print($e->getTraceAsString() . "</br>" . $e->getMessage());
-//    }
-    
