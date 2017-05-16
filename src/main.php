d9e67f5b2b50932a0d38ea6a7ac4a91fd9fa4c32
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2017-05-16 09:09:20 -0500

    Revisión de versión, revisión de comentarios.

--- src/main.php
+++ src/main.php
@@ -9,12 +9,18 @@
  * License, or (at your option) any later version.
  */
 
-namespace GIgenerator;
+namespace GIndie\Generator\DML;
 
-namespace GIgenerator\DML;
+spl_autoload_register(function($className) {
+    $edited = substr($className,
+            strlen(__NAMESPACE__) + strrpos($className, __NAMESPACE__));
+    $edited = str_replace("\\", \DIRECTORY_SEPARATOR, __DIR__ . $edited) . ".php";
+    if (is_readable($edited)) {
+        require_once($edited);
+    }
+});
 
-require_once __DIR__ . '/common.php';
-require_once __DIR__ . '/main/Node.php';
+require_once __DIR__ . '/main/common.php';
 
 /**
  * Factory Pattern for a <b>Descriptive Markup Languaje</b> (<b>DML</b>).
@@ -23,12 +29,11 @@ require_once __DIR__ . '/main/Node.php';
  * this class (more info. at <https://en.wikipedia.org/wiki/Markup_language>).
  * 
  * @package     DML
- * @subpackage  Node
  * @category    API
  * 
  * @copyright   (c) 2017 Angel Sierra Vega. Grupo INDIE.
  *
- * @version     GI-DML.01
+ * @version     GIG-DML.01.02
  * @since       2016-12-16
  * @author      Angel Sierra Vega <angel.sierra@grupoindie.com>
  * 
@@ -38,15 +43,17 @@ class Node extends Node\Node {
     /**
      * Creates a simple DML node.
      * 
-     * @param   $tag
-     * @param   array $attributes [optional]
+     * @param   string $tagname The name of the tag.
+     * @param   array $attributes [optional] An associative array where 
+     *              key = Attribute name and value = The literal value of the attribute.
+     *                  
      * @param   array $content [optional]
      * 
-     * @return  Node\Node An object representation of a DML node.
+     * @return  \GIndie\Generator\DML\Node\Node An object representation of a DML node.
      * 
      * @example examples/01-Node-Creation.php Example 1: Simple node.
      *  <pre>  
-     *      echo GIgenerator\DML\Node::Simple("node_simple");
+     *      echo GIndie\Generator\DML\Node::Simple("node_simple");
      * </pre> 
      *  <i><pre>
      *      <node_simple></node_simple>
@@ -54,7 +61,7 @@ class Node extends Node\Node {
      * 
      * @example examples/01-Node-Creation.php Example 8: Node with attribute-value.
      *  <pre>  
-     *      echo GIgenerator\DML\Node::Simple("node",["attr"=>"val"]);
+     *      echo GIndie\Generator\DML\Node::Simple("node",["attr"=>"val"]);
      * </pre> 
      *  <i><pre>
      *      <node attr='val'></node>
@@ -62,7 +69,7 @@ class Node extends Node\Node {
      * 
      * @example examples/01-Node-Creation.php Example 5: Node with text content.
      *  <pre>  
-     *      echo GIgenerator\DML\Node::Simple("node",[],["content"]);
+     *      echo GIndie\Generator\DML\Node::Simple("node",[],["content"]);
      * </pre> 
      *  <i><pre>
      *      <node>content</node>
@@ -70,18 +77,18 @@ class Node extends Node\Node {
      * 
      * @example examples/01-Node-Creation.php Example 6: Node with nested node.
      *  <pre>  
-     *      echo GIgenerator\DML\Node::Simple("parent",[],[GIgenerator\DML\Node::Simple("child")]);
+     *      echo GIndie\Generator\DML\Node::Simple("parent",[],[GIgenerator\DML\Node::Simple("child")]);
      * </pre> 
      *  <i><pre>
      *      <parent><child></child></parent>
      *  </pre></i>
      * 
-     * @version     GI-DML.01.02
-     * @since       2016-12-16
+     * @version     GIG-DML.01.02
      * @author      Angel Sierra Vega <angel.sierra@grupoindie.com>
      */
-    public static function Simple($tag, array $attributes = [], array $content = []) {
-        return new static($tag, false, $attributes, $content);
+    public static function Simple($tagname, array $attributes = [],
+            array $content = []) {
+        return new static($tagname, false, $attributes, $content);
     }
 
     /**
@@ -94,14 +101,13 @@ class Node extends Node\Node {
      * 
      * @example examples/01-Node-Creation.php Example 3: Closed tag.
      *  <pre>  
-     *      GIgenerator\DML\Node::Closed("node_closed");
+     *      GIndie\Generator\DML\Node::Closed("node_closed");
      * </pre> 
      *  <i><pre>
      *      <node_closed />
      *  </pre></i>
      * 
-     * @version     GI-DML.01.02
-     * @since       2017-03-28
+     * @version     GIG-DML.01.02
      * @author      Angel Sierra Vega <angel.sierra@grupoindie.com>
      */
     public static function Closed($tag, array $attributes = []) {
@@ -117,14 +123,13 @@ class Node extends Node\Node {
      * 
      * @example     examples/01-Node-Creation.php Example 4: Content only node.
      *  <pre>  
-     *      echo GIgenerator\DML\Node::ContentOnly([GIgenerator\DML\Node::Simple("content1"),GIgenerator\DML\Node::Simple("content2")]);
+     *      echo GIndie\Generator\DML\Node::ContentOnly([GIgenerator\DML\Node::Simple("content1"),GIgenerator\DML\Node::Simple("content2")]);
      * </pre> 
      *  <i><pre>
      *      <content1></content1><content2></content2>
      *  </pre></i>
      * 
-     * @version     GI-DML.01.02
-     * @since       2016-12-21
+     * @version     GIG-DML.01.02
      * @author      Angel Sierra Vega <angel.sierra@grupoindie.com>
      */
     public static function ContentOnly(array $content) {
@@ -141,14 +146,13 @@ class Node extends Node\Node {
      * 
      * @example     examples/01-Node-Creation.php Example 2: Empty node (open tag only).
      *  <pre>  
-     *      echo GIgenerator\DML\Node::EmptyNode("node_empty");
+     *      echo GIndie\Generator\DML\Node::EmptyNode("node_empty");
      * </pre> 
      *  <i><pre>
      *      <node_empty>
      *  </pre></i>
      * 
-     * @version     GI-DML.01.02
-     * @since       2016-12-19
+     * @version     GIG-DML.01.02
      * @author      Angel Sierra Vega <angel.sierra@grupoindie.com>
      */
     public static function EmptyNode($tag, array $attributes = []) {
